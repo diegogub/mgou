27b3bf5eb7916b37b9111c13c74aa7dca962b558
Diego Guraieb <diegogub@gmail.com>
2015-04-08 17:27:19 -0300

    updated to new v2 mgo driver

--- mgou.go
+++ mgou.go
@@ -1,27 +1,27 @@
 package mgou
 
-import(
-    "labix.org/v2/mgo"
-    "errors"
+import (
+	"errors"
+	"gopkg.in/mgo.v2"
 )
 
-var(
-    mgos *mgo.Session // mongo session to mgou
+var (
+	mgos *mgo.Session // mongo session to mgou
 )
 
-func Mongo() *mgo.Session{
-    return mgos.Clone()
+func Mongo() *mgo.Session {
+	return mgos.Clone()
 }
 
-func Init(s *mgo.Session) error{
-    if s == nil{
-        return errors.New("Invalid session to init mgou")
-    }
-    // Ping to server if availale
-    err := s.Ping()
-    if err != nil{
-        return err
-    }
-    mgos = s
-    return nil
+func Init(s *mgo.Session) error {
+	if s == nil {
+		return errors.New("Invalid session to init mgou")
+	}
+	// Ping to server if availale
+	err := s.Ping()
+	if err != nil {
+		return err
+	}
+	mgos = s
+	return nil
 }
--- query.go
+++ query.go
@@ -1,25 +1,25 @@
 package mgou
 
-import(
-    "reflect"
-    "labix.org/v2/mgo/bson"
+import (
+	"gopkg.in/mgo.v2/bson"
+	"reflect"
 )
 
 type Query struct {
-    Q       interface{}   `json:"-"`
-    Count   int           `json:"total"`
-    Page    int           `json:"page"`
-    Limit   int           `json:"limit"`
-    Result  interface{}   `json:"result"`
+	Q      interface{} `json:"-"`
+	Count  int         `json:"total"`
+	Page   int         `json:"page"`
+	Limit  int         `json:"limit"`
+	Result interface{} `json:"result"`
 }
 
-func NewQuery() *Query{
-    var q Query
-    Q := bson.M{}
-    q.Q = Q
-    return &q
+func NewQuery() *Query {
+	var q Query
+	Q := bson.M{}
+	q.Q = Q
+	return &q
 }
 
-func (q *Query) Like(m Modeler){
-   q.Q = reflect.ValueOf(m).Interface()
+func (q *Query) Like(m Modeler) {
+	q.Q = reflect.ValueOf(m).Interface()
 }
